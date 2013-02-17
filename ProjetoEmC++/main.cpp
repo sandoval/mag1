f4d9f55e8908319a2bcf5205b2051e57f5ad9653
vitorecomp <vitor.ecomp@gmail.com>
2013-02-16 23:49:47 -0300

    Porjeto em C++ para teste

---
+++ ProjetoEmC++/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Vitor
+ *
+ * Created on 16 de Fevereiro de 2013, 23:49
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
