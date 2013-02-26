12b4a15b140951191aaaa3ef1b36b2309c23c7dd
Daniel Sandoval <daniel@loopec.com.br>
2013-02-26 12:32:50 -0300

    Código frenético que faz amostragem e mostra o sinal e distribuição no espectro da frequência!

--- Untitled.m
+++ Untitled.m
@@ -1,18 +1,60 @@
 clear all
+close all
 clc
 
-s = serial('COM35');
+s = serial('/dev/tty.usbmodemfd121');
 fopen(s);
 
 reads = 800;
 
-a = zeros(1,reads);
+sinal = zeros(1,reads);
 pause(5);
 for i=1:reads
    fprintf(s,'a');
-    a(i) = str2double(fscanf(s));
+    sinal(i) = str2double(fscanf(s));
     
 end
 
 fclose(s);
 delete(s);
+
+figure;plot(sinal)
+title('Sinal de entrada')
+amostras = size(sinal,2);
+
+Fs = 470; 
+
+x = sinal-mean(sinal); 
+
+
+% Adiciona padding para fazer a FFT.
+nfft= 2^(nextpow2(length(x))); 
+fftx = fft(x,nfft); 
+
+
+% Calcula o n?mero de pontos ?nicos
+NumUniquePts = ceil((nfft+1)/2); 
+
+
+% Corta pela metade a fft, pois ? sim?trica
+fftx = fftx(1:NumUniquePts); 
+
+
+% Escala a fft para que n?o seja uma fun??o do tamanho da amostra
+mx = abs(fftx)/length(x); 
+mx = mx.^2; 
+
+if rem(nfft, 2) % odd nfft excludes Nyquist point
+  mx(2:end) = mx(2:end)*2;
+else
+  mx(2:end -1) = mx(2:end -1)*2;
+end
+
+f = (0:NumUniquePts-1)*Fs/nfft;
+
+% Generate the plot, title and labels. 
+
+figure;plot(f,mx); 
+title('Magnitude do sinal no espectro da frequ?ncia'); 
+xlabel('Frequ?ncia (Hz)'); 
+ylabel('Magnitude'); 
