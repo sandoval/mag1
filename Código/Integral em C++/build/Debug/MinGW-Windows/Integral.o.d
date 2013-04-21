e05179aaab98e668d4c60329bc8311749975789e
vitorecomp <vitor.ecomp@gmail.com>
2013-04-20 22:32:36 -0300

    mudo o nome da pasta

--- Código/Integral em C++/build/Debug/MinGW-Windows/Integral.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/MinGW-Windows/Integral.o: Integral.cpp Integral.h \
- TiposBasicos.h tiposbasicos1.h
-
-Integral.h:
-
-TiposBasicos.h:
-
-tiposbasicos1.h:
--- Código/Integral em C++/build/Debug/MinGW-Windows/main.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/MinGW-Windows/main.o: main.cpp Interface.h
-
-Interface.h:
